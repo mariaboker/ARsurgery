4c2ac98f5c689e18f2e1d27ba88eebaa2dd7425a
Maria <mariaboker@hotmail.com>
2018-07-11 03:43:13 -0400

    Unity Scripts
    Augmented Reality part of the project coded in Unity .

---
+++ Bluetooth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Bluetooth : MonoBehaviour {
+
+    SerialPort serial;
+
+	// Use this for initialization
+	void Start () {
+
+         serial = new SerialPort("COM6", 9600);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+
+public class Rotator : MonoBehaviour
+{
+
+    SerialPort serial = new SerialPort("COM6", 9600);
+    void Update()
+    {
+
+        if (!serial.IsOpen)
+            serial.Open();
+
+        int rotation = int.Parse(serial.ReadLine());
+        transform.localEulerAngles = new Vector3(0, rotation, 0);
+
+
+
+
+    }
+}
+
---
+++ webCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class webCamera : MonoBehaviour
+{
+    public GameObject webPlane;
+
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(i + " " + devices[i].name);
+        }
+
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+
+        WebCamTexture webcamTexture = new WebCamTexture(devices[1].name);//, Screen.width, Screen.height);
+        webPlane.GetComponent<MeshRenderer>().material.mainTexture = webcamTexture;
+
+        // rend.material.mainTexture = webcamTexture; if you dont wanna use a plane, try this
+
+        webcamTexture.Play();
+    }
+
+    //public RawImage background;
+
+    //private void Start()
+    //{
+    //    WebCamDevice[] devices = WebCamTexture.devices;
+
+    //    Renderer rend = this.GetComponentInChildren<Renderer>();
+
+    //    WebCamTexture webCamRaw = new WebCamTexture(devices[0].name, Screen.width, Screen.height);
+    //    background.GetComponent<MeshRenderer>().material.mainTexture = webCamRaw;
+
+    //    webCamRaw.Play();
+    //}
+
+}
